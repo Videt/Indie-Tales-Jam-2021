62236551ccc28ad6d7c7d50c12afe38204c50da7
Videt <egor310501@mail.ru>
2021-07-11 02:18:12 +1100

    added a timer on a blood stain that restarts the level

---
+++ Indie-Tales-Jam-2021/Assets/Scripts/BloodSplash/BloodSplash.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BloodSplash : MonoBehaviour
+{
+    [SerializeField] private int endGameDelay;
+    [SerializeField] private Text textField;
+
+    private void Start()
+    {
+        StartCoroutine(EndGameTimer());
+    }
+
+    IEnumerator EndGameTimer()
+    {
+        while (endGameDelay >= 0)
+        {
+            textField.text = endGameDelay.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            endGameDelay--;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
