f8cfe0b7521d13d3c64edd87c2abff6958f87bf3
Videt <egor310501@mail.ru>
2021-07-10 22:24:35 +1100

    added movement and shooting

---
+++ Indie-Tales-Jam-2021/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement")]
+    [SerializeField] private Rigidbody2D playerRb2d;
+    [SerializeField] private float moveSpeed;
+    private Vector2 movement;
+
+    [Header("Shooting setup")]
+    //[SerializeField] private Transform bulletTransform;
+    [SerializeField] private Camera cam;
+
+    [Header("Fire")]
+    private Transform playerFirePointTransform;
+    [SerializeField] private Rigidbody2D playerFirePointRb;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float reloadTime;
+    [SerializeField] private GameObject bulletPrefab;
+    private bool canShoot = true;
+    private bool isShooting;
+    private Vector2 mousePos;
+    private Vector2 aimDirection;
+    private Vector2 lookDirection;
+
+    [Header("Clean")]
+    private bool canClean;
+    private bool isCleaning;
+
+    private void Start()
+    {
+        playerFirePointTransform = playerFirePointRb.transform;
+    }
+
+    private void Update()
+    {
+        MouseLook();
+
+        if (canShoot && isShooting)
+        {
+            Shoot();
+            canShoot = false;
+            Invoke(nameof(Reload), reloadTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        playerRb2d.AddForce(moveSpeed * Time.deltaTime * movement, ForceMode2D.Impulse);
+    }
+
+    private void Shoot()
+    {
+        print("Shoot");
+
+        GameObject bullet = Instantiate(bulletPrefab, playerFirePointTransform.position, playerFirePointTransform.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(playerFirePointTransform.up * bulletSpeed, ForceMode2D.Impulse);
+    }
+
+    private void Clean()
+    {
+
+    }
+
+    private void Reload()
+    {
+        canShoot = true;
+    }
+
+    private void MouseLook()
+    {
+        mousePos = cam.ScreenToWorldPoint(aimDirection);
+        lookDirection = mousePos - playerFirePointRb.position;
+        float angle = (Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg) - 90f;
+        playerFirePointTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        movement = value.ReadValue<Vector2>();
+    }
+
+    public void OnAimDirection(InputAction.CallbackContext value)
+    {
+        aimDirection = value.ReadValue<Vector2>();
+    }
+
+    public void OnShoot(InputAction.CallbackContext value)
+    {
+        if (value.performed)
+        {
+            //targetPos = new Vector3(cam.ScreenToWorldPoint(aimDirection).x, cam.ScreenToWorldPoint(aimDirection).y, 0);
+            isShooting = true;
+        }
+
+        if (value.canceled)
+            isShooting = false;
+    }
+
+    public void OnClean(InputAction.CallbackContext value)
+    {
+        if (canClean && value.performed)
+        {
+            isCleaning = true;
+            canClean = false;
+            print("Clean");
+        }
+    }
+}
