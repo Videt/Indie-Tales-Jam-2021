ce5e7e4849496ebc14836bc36cb3189701ee4ab4
Videt <egor310501@mail.ru>
2021-07-11 17:37:54 +1100

    bullet and enemy sprites

--- Indie-Tales-Jam-2021/Assets/Scripts/Bullet/Bullet.cs
+++ Indie-Tales-Jam-2021/Assets/Scripts/Bullet/Bullet.cs
@@ -6,7 +6,7 @@ public class Bullet : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Player") && !collision.CompareTag("BloodSplash"))
+        if (!collision.CompareTag("Player") && !collision.CompareTag("BloodSplash") && !collision.CompareTag("Bullet"))
         {
             Destroy(gameObject);
         }
