cd2edd7453f5e64529aa4ceb3b67c2f1d4272263
Videt <egor310501@mail.ru>
2021-07-11 19:06:33 +1100

    added music

---
+++ Indie-Tales-Jam-2021/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (var sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("MainTheme");
+    }
+
+    public void Play(string clipName)
+    {
+        Sound s = Array.Find(sounds, sound => sound.clipName == clipName);
+        s.source.Play();
+    }
+}
