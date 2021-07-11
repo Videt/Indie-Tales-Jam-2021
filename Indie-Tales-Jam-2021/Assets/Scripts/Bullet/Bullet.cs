a15143253109e855316f4c8004c62ac73f8fac79
Videt <egor310501@mail.ru>
2021-07-11 17:10:10 +1100

    added a pixel perfect camera, walls + some tweaking

--- Indie-Tales-Jam-2021/Assets/Scripts/Bullet/Bullet.cs
+++ Indie-Tales-Jam-2021/Assets/Scripts/Bullet/Bullet.cs
@@ -6,7 +6,7 @@ public class Bullet : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (!collision.CompareTag("Player") && !collision.CompareTag("BloodSplash"))
         {
             Destroy(gameObject);
         }
